309eb44f04b3c52f0aa0955311770ffd112ec1a0
liam <liam@merkel.id.au>
2024-12-29 10:08:17 +1100

    moved part one to function

--- day1/d1.rb
+++ day1/d1.rb
@@ -1,3 +1,42 @@
+def getDistances (dataA, dataB) 
+  foundcount = 0
+  foundIndexesA = []
+  foundIndexesB = []
+  distances = []
+  
+  while foundcount != dataA.length() 
+    curA = nil
+    curB = nil
+    curFIA = 0
+    curFIB = 0
+    for i in 0..(dataA.length()-1)
+      if (!foundIndexesA.include? i) && ( curA == nil || dataA[i] < curA)
+        curA = dataA[i]
+        curFIA = i
+      end
+      if (!foundIndexesB.include? i) && ( curB == nil || dataB[i] < curB)
+        curB = dataB[i]
+        curFIB = i
+      end
+    end 
+    foundIndexesA.push(curFIA)
+    foundIndexesB.push(curFIB)
+    foundcount += 1
+    dist = curA - curB
+    if dist < 0 
+      dist *= -1
+    end
+    distances.push dist
+  end
+  puts "found Count: " + foundcount.to_s
+  total = 0
+  for i in distances
+    total += i
+  end
+  return total
+end
+
+
 input = File.read("data")
 
 lines = input.split("\n")
@@ -13,37 +52,4 @@ end
 puts "Done reading into arrays"
 puts "dA: " + dataA.length().to_s
 puts "dB: " + dataB.length().to_s
-foundcount = 0
-foundIndexesA = []
-foundIndexesB = []
-distances = []
-while foundcount != dataA.length() 
-  curA = nil
-  curB = nil
-  curFIA = 0
-  curFIB = 0
-  for i in 0..(dataA.length()-1)
-    if (!foundIndexesA.include? i) && ( curA == nil || dataA[i] < curA)
-      curA = dataA[i]
-      curFIA = i
-    end
-    if (!foundIndexesB.include? i) && ( curB == nil || dataB[i] < curB)
-      curB = dataB[i]
-      curFIB = i
-    end
-  end 
-  foundIndexesA.push(curFIA)
-  foundIndexesB.push(curFIB)
-  foundcount += 1
-  dist = curA - curB
-  if dist < 0 
-    dist *= -1
-  end
-  distances.push dist
-end
-puts "found Count: " + foundcount.to_s
-total = 0
-for i in distances
-  total += i
-end
-puts total
+puts "Distance: " + getDistances(dataA, dataB).to_s 
